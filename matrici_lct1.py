b0319e39fb7215d6af0491994bd825e1c2e5d1ba
Loredana <70909576+loredana-stirbu@users.noreply.github.com>
2021-10-06 20:29:27 +0300

    Add files via upload

---
+++ matrici_lct1.py
@@ -0,0 +1,34 @@
+matrice=[[34,17,4,89,3],
+  [11,26,49,54,9],
+  [8,44,37,21,35],
+  [20,42,19,73,10],
+  [56,47,36,1,17]]
+
+s=[]
+for i in range(len(matrice)):
+    s.append(sum(matrice[i]))
+print("Suma elementelor de pe fiecare linie:",s)
+
+suma=[]
+coloana=[]
+for j in range(len(matrice[0])):
+    for rind in matrice:
+        coloana.append(rind[j])
+    suma.append(sum(coloana))
+print("Suma elementelor de pe fiecare coloana:",suma)
+
+diagonala_principala=[]
+for i in range(len(matrice)):
+    for j in range(len(matrice[0])) :
+        if i==j :
+            diagonala_principala.append(matrice[i][j])
+print("Diagonala principala:",diagonala_principala)
+
+diagonala_secundara=[] 
+for i in range(len(matrice)):
+    for j in range(len(matrice[0])) :
+        n=5
+        if i+j==n-1:
+            diagonala_secundara.append(matrice[i][j])
+            
+print("Diagonala secundara:",diagonala_secundara) 
